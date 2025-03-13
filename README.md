ce907e35e27a167459e0a6609cd4240ffbf7cc31
Chad Bentz <1760475+felickz@users.noreply.github.com>
2025-03-13 11:14:14 -0400

    Add CodeQL Fixer Report GitHub Action and related files

---
+++ README.md
@@ -0,0 +1,55 @@
+# felickz/codeql-fixer-report GitHub Action
+
+This action allows you to generate a CodeQL fixer report.
+
+This action queries the CodeQL API to get a list of users who are fixing alerts in your repositories, producing a CSV report that summarizes the number of fixes by each developer.
+
+## Usage
+
+To use the `felickz/codeql-fixer-report` action, you need to set it up in a workflow file (`.github/workflows/codeql-fixer-report.yml`).
+
+Here's a basic example:
+
+```yaml
+name: CodeQL Report
+
+on:
+  push:
+    paths:
+      - '.github/workflows/codeql-fixer-report.yml'
+  workflow_dispatch:
+  #every 12 hours
+  schedule:
+    - cron: '0 */12 * * *'
+
+jobs:
+  run-report:
+    runs-on: ubuntu-latest
+
+    steps:
+    - name: Use felickz/codeql-fixer-report action
+      uses: felickz/codeql-fixer-report@main
+      with:
+        token: ${{ secrets.GITHUB_TOKEN }}
+    - name: Upload CodeQL Report CSV as Artifact
+      uses: actions/upload-artifact@v4
+      with:
+        name: "CodeQLFixersReport-${{ github.run_id }}"
+        path: ./*.csv
+```
+
+In this example, the felickz/codeql-fixer-report action is used from the `main` branch directly.  The report is run every 12 hours via cron schedule.
+
+The github-token input is required for the felickz/codeql-fixer-report action. It uses the GITHUB_TOKEN secret, which would need to have `metadata:read` and `actions:read` permissions for your organization for any private repos.
+
+The `upload-artfact` action is used to create the CSV attached to the action workflow summary.
+
+## Inputs
+### token
+Required The GitHub token to authenticate and pull CodeQL Action workflow status with. Needs to have `metadata:read` and `actions:read` permissions for your organization's private repos.
+
+### organization
+Optional The GitHub Organization. Defaults to the current Organization.
+
+### repository
+Optional The GitHub Repository. Defaults to the current Repository from the workflow context.
---
+++ action.ps1
@@ -0,0 +1,117 @@
+<#
+.SYNOPSIS
+    This script retrieves the CodeQL workflow runs for repositories in a specified GitHub organization and generates a summary report in Markdown format.
+
+.DESCRIPTION
+    The report includes the conclusion, workflow URL, whether the workflow is the default one, organization name, repository name, and workflow path. The script also checks if the FormatMarkdownTable module is installed and installs it if necessary. Finally, it converts the summary report to Markdown format and outputs it to the GITHUB_STEP_SUMMARY environment variable. If the script is not running in a GitHub Actions environment, it also outputs the summary report to the console.
+
+.PARAMETER GitHubToken
+    The GitHub PAT that is used to authenticate to GitHub GH CLI (uses the envioronment value GH_TOKEN).
+
+.PARAMETER GitHubOrganization
+    The GitHub organization that the script will operate on. Defaults to the current Organization if not provided.
+
+.PARAMETER GitHubRepository
+    The GitHub repository that the script will operate on. Defaults to the current Repository if not provided.
+
+.EXAMPLE
+    .\action.ps1 -GitHubToken "your_token_here" -GitHubOrganization "your_organization_here" -GitHubRepository "your_repository_here"
+    
+    .\action.ps1 -GitHubToken (gh auth token) -GitHubOrganization "octodemo" -GitHubRepository "old-vulnerable-node"
+
+.NOTES
+    Be careful not to expose your GitHub PAT in your workflow or any public places because it can be used to access your GitHub account.
+
+.LINK
+    For more information about GitHub Actions, visit: https://docs.github.com/en/actions.
+    For information about composite actions, visit: https://docs.github.com/en/actions/creating-actions/creating-a-composite-action.
+    For information about the GH CLI, visit: https://cli.github.com/manual/gh_api.
+
+#>
+
+param(
+    [string]$GitHubToken,
+    [string]$GitHubOrganization,
+    [string]$GitHubRepository
+)
+
+# set the GH_TOKEN environment variable to the value of the GitHubToken parameter
+if (![String]::IsNullOrWhiteSpace($GitHubToken)) {
+    $env:GH_TOKEN = $GitHubToken
+}
+
+# Set GitHubOrganization from GITHUB_REPOSITORY_OWNER environment variable if not already set
+if ([String]::IsNullOrWhiteSpace($GitHubOrganization)) {
+    if ($null -ne $env:GITHUB_REPOSITORY_OWNER) {
+        $GitHubOrganization = $env:GITHUB_REPOSITORY_OWNER
+    }
+}
+
+# Set GitHubRepository from GITHUB_REPOSITORY environment variable if not already set
+if ([String]::IsNullOrWhiteSpace($GitHubRepository)) {
+    if ($null -ne $env:GITHUB_REPOSITORY) {
+        # GITHUB_REPOSITORY is in the format "owner/repo", we just need the repo part
+        $GitHubRepository = $env:GITHUB_REPOSITORY.Split('/')[1]
+    }
+}
+
+$nwo = "$GitHubOrganization/$GitHubRepository"
+$state = "fixed" # NOTE - fixed means alert was not present in a subsequent scan - which might introduce noise if a code scanning config was removed
+$alerts = gh api "/repos/$nwo/code-scanning/alerts?state=$state" --paginate | ConvertFrom-Json
+
+# Loop through each alert and build a list of users who fixed them and keep a count
+$commitCache = @{}
+$fixers = @{}
+foreach ($alert in $alerts) {    
+    # Get the commit details from cache or API
+    $sha = $alert.most_recent_instance.commit_sha
+    if (-not $commitCache.ContainsKey($sha)) {
+        $commitCache[$sha] = gh api "/repos/$nwo/git/commits/$sha" | ConvertFrom-Json
+    }
+
+    $commit = $commitCache[$sha]
+    $author = $commit.author.name
+    $email = $commit.author.email
+    #Write-Host "#$($alert.number) - $($alert.state) RULE:$($alert.rule.id) SHA:$sha Author: $($commit.author.name) Date:$($commit.author.date)"
+
+    if ($fixers.ContainsKey($author)) {
+        $fixers[$author].Count++
+    }
+    else {
+        $fixers[$author] = @{
+            Count = 1
+            Email = $email
+            Org = $GitHubOrganization
+            Repo = $GitHubRepository
+        }
+    }
+}
+
+$csv = "CodeQLFixersReport.csv"
+$header = "Author,Email,Org,Repo,NumFixes"
+Set-Content -Path "./$csv" -Value $header
+
+# Write results to CSV file
+foreach ($author in $fixers.Keys) {
+    $line = "$author,$($fixers[$author].Email),$($fixers[$author].Org),$($fixers[$author].Repo),$($fixers[$author].Count)"
+    Add-Content -Path "./$csv" -Value $line
+}
+
+#TODO move to a manifest like choco package.config
+if (Get-Module -ListAvailable -Name FormatMarkdownTable -ErrorAction SilentlyContinue) {
+    Write-Host "FormatMarkdownTable module is installed"
+}
+else {
+    # Handle `Untrusted repository` prompt
+    Set-PSRepository PSGallery -InstallationPolicy Trusted
+    #directly to output here before module loaded to support Write-ActionInfo
+    Write-Host "FormatMarkdownTable module is not installed.  Installing from Gallery..."
+    Install-Module -Name FormatMarkdownTable
+}
+
+$markdownSummary = Import-Csv -Path "./$csv" | Format-MarkdownTableTableStyle -ShowMarkdown -DoNotCopyToClipboard -HideStandardOutput
+$markdownSummary > $env:GITHUB_STEP_SUMMARY
+
+if ($null -eq $env:GITHUB_ACTIONS) {
+    $markdownSummary
+}
